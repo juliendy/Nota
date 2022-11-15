de7680183bfae98c0e4856aac49e1aef35506e91
juliendy <julien.yasar@gmail.com>
2022-11-15 21:54:35 +0100

    feat: routes created. react-select init for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NewNote } from "./NewNote"
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<h1>Hi!</h1>}/>
-  </Routes>
+    return (
+        <Container className="my-4">
+            <Routes>
+                <Route path="/" element={<h1>Hi!</h1>} />
+                <Route path="/new" element={<NewNote />} />
+                <Route path="/:id">
+                    <Route index element={<h1>Show</h1>} />
+                    <Route path="edit" element={<h1>Edit</h1>} />
+                </Route>
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Container>
+    );
 }
 
-export default App
+export default App;
