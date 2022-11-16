0f0a9f73594bf5a6052ac28f019cb95a63eb959d
juliendy <julien.yasar@gmail.com>
2022-11-16 14:06:33 +0100

    feat: conditional logic and types added to noteform

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NewNote } from "./NewNote"
 
+export type Note = {
+    id: string
+} & NoteData
+
+export type NoteData = {
+    title: string,
+    markdown: string,
+    tags: Tag[],
+}
+
+export type Tag = {
+    id: string,
+    label: string
+}
+
 function App() {
     return (
         <Container className="my-4">
             <Routes>
-                <Route path="/" element={<h1>Hi!</h1>} />
+                <Route path="/" element={<h1>Home</h1>} />
                 <Route path="/new" element={<NewNote />} />
                 <Route path="/:id">
                     <Route index element={<h1>Show</h1>} />
